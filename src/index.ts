20edde8254ffc12bd60349fdf952295b96f1df8c
James Smith <jamesplease2@gmail.com>
2018-07-30 16:27:44 -0700

    Convert linear-scale to TS; add back in tests

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
 export { default as clamp } from './clamp';
+export { default as linearScale } from './linear-scale';
+// export * from './viewport';
+// export * from './timeline';
+// export * from './zoom-bar';
+// export * from './scroll-and-zoom';
+// export * from './conversions';
--- src/linear-scale.ts
+++ src/linear-scale.ts
@@ -1,13 +1,21 @@
+interface LinearScaleOptions {
+  domain: number[];
+  range: number[];
+  value: number;
+}
+
 // A linear map of `value` from `domain` onto `range`.
 // Linear maps can be represented as linear functions.
 // In this case, we are putting our inputs (which are two coordinates)
 // into two-point form:
 // https://en.wikipedia.org/wiki/Linear_equation#Two-point_form
-export default function linearScale({
-  domain = [0, 1],
-  range = [0, 1],
-  value = 0,
-} = {}) {
+export default function linearScale(
+  { domain = [0, 1], range = [0, 1], value = 0 }: LinearScaleOptions = {
+    domain: [0, 1],
+    range: [0, 1],
+    value: 0,
+  }
+): number {
   if (domain[0] === domain[1]) {
     return domain[0];
   }
