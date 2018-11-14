1ed02f9622ddb294f41a1a55563a522a3370893e
Jmeas <james_please@icloud.com>
2018-11-13 22:37:22 -0800

    Attempt to transpile function parameters features

--- .babelrc.js
+++ .babelrc.js
@@ -15,6 +15,7 @@ module.exports = {
     ],
   ],
   plugins: [
+    '@babel/plugin-transform-parameters',
     // don't use `loose` mode here - need to copy symbols when spreading
     '@babel/proposal-object-rest-spread',
     NODE_ENV === 'test' && '@babel/transform-modules-commonjs',
