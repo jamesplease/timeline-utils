bf77626b97c816be0c32998e07d5e9fed7725387
Jmeas <james_please@icloud.com>
2018-11-13 22:48:21 -0800

    Transpile source down to ES5 :

--- .babelrc.js
+++ .babelrc.js
@@ -1,23 +1,10 @@
 const { NODE_ENV } = process.env;
 
+// Heads up! The TypeScript compiler handles most of the transpiling.
+// To configure what sorts of features get transpiled away, refer to `tsconfig.json`
+
 module.exports = {
-  presets: [
-    [
-      '@babel/env',
-      {
-        targets: {
-          browsers: ['ie >= 11'],
-        },
-        exclude: ['transform-async-to-generator', 'transform-regenerator'],
-        modules: false,
-        loose: true,
-      },
-    ],
-  ],
-  plugins: [
-    '@babel/plugin-transform-parameters',
-    // don't use `loose` mode here - need to copy symbols when spreading
-    '@babel/proposal-object-rest-spread',
-    NODE_ENV === 'test' && '@babel/transform-modules-commonjs',
-  ].filter(Boolean),
+  plugins: [NODE_ENV === 'test' && '@babel/transform-modules-commonjs'].filter(
+    Boolean
+  ),
 };
